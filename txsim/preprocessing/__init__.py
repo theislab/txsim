f73d176f7ef6bc54ec344a06b39f17de35935473
Habib Rehman <hrehma6@illinois.edu>
2022-08-01 14:38:51 +0200

    Seperated segmentation functions

--- txsim/preprocessing/__init__.py
+++ txsim/preprocessing/__init__.py
@@ -1,2 +1,2 @@
 from ._normalization import normalize_total, normalize_pearson_residuals, normalize_by_area
-from ._segmentation import segment_nuclei
+from ._segmentation import segment_nuclei, segment_cellpose
