a5a804ebee2e2c4e9309564c3d758e5a7c12f262
danielStrobl <daniel.strobl@helmholtz-muenchen.de>
2022-09-21 16:25:39 +0200

    normalisation api change + normalise sc data

--- txsim/preprocessing/__init__.py
+++ txsim/preprocessing/__init__.py
@@ -1,4 +1,5 @@
 from ._normalization import normalize_total, normalize_pearson_residuals, normalize_by_area
+from .normalization_sc import normalize_sc
 from ._segmentation import segment_nuclei, segment_cellpose
 from ._assignment import basic_assign, run_pciSeq, run_clustermap
 from ._countgeneration import generate_adata, calculate_alpha_area
