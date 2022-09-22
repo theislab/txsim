2862461251bafa730911e6f32a3ce1ffc12056b5
LuckyMD <m.d.luecken@gmail.com>
2022-09-22 12:05:52 +0200

    add metric to combined script

--- txsim/metrics/_combined.py
+++ txsim/metrics/_combined.py
@@ -29,17 +29,20 @@ def all_metrics(
     # Expression similarity metrics
     metrics['gene_exp_similarity'] = similar_ge_across_clusters(adata_sp,adata_sc)
     metrics['mean_sim_across_clust'] = mean_similarity_gene_expression_across_clusters(adata_sp,adata_sc)
-    metrics['prc95_sim_across_clust']=percentile95_similarity_gene_expression_across_clusters(adata_sp,adata_sc)
+    metrics['prc95_sim_across_clust'] = percentile95_similarity_gene_expression_across_clusters(adata_sp,adata_sc)
+    # Coexpression similarity
+    metrics['gene_set_coexpression'] = gene_set_coexpression(adata_sp, adata_sc)
     # Negative marker purity
-    metrics['neg_marker_purity']=negative_marker_purity(adata_sp,adata_sc)
+    metrics['neg_marker_purity'] = negative_marker_purity(adata_sp,adata_sc)
     # Cell statistics
-    metrics['ratio_median_readsxcell']=ratio_median_readsXcells(adata_sp,adata_sc)
-    metrics['ratio_mean_readsxcell']=ratio_mean_readsXcells(adata_sp,adata_sc)
-    metrics['ratio_n_cells']=ratio_number_of_cells(adata_sp,adata_sc)
-    metrics['ratio_mean_genexcells']=ratio_mean_genesXcells(adata_sp,adata_sc)
-    metrics['ratio_median_genexcells']=ratio_median_genesXcells(adata_sp,adata_sc)
+    metrics['ratio_median_readsxcell'] = ratio_median_readsXcells(adata_sp,adata_sc)
+    metrics['ratio_mean_readsxcell'] = ratio_mean_readsXcells(adata_sp,adata_sc)
+    metrics['ratio_n_cells'] = ratio_number_of_cells(adata_sp,adata_sc)
+    metrics['ratio_mean_genexcells'] = ratio_mean_genesXcells(adata_sp,adata_sc)
+    metrics['ratio_median_genexcells'] = ratio_median_genesXcells(adata_sp,adata_sc)
     # KNN mixing
-    metrics['knn_mixing']=knn_mixing(adata_sp,adata_sc)
+    metrics['knn_mixing'] = knn_mixing(adata_sp,adata_sc)
+
     
     
     
