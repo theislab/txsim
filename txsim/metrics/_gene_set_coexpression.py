5e98afc5af26f2e0d0659011ff7a9e6d2248fab0
LuckyMD <m.d.luecken@gmail.com>
2022-09-22 13:01:26 +0200

    add min_cell expression to gene_set metric

--- txsim/metrics/_gene_set_coexpression.py
+++ txsim/metrics/_gene_set_coexpression.py
@@ -8,6 +8,7 @@ def gene_set_coexpression(
     spatial_data: AnnData,
     seq_data: AnnData,
     overlap_threshold: int = 5,
+    min_cells: int = 20,
     pipeline_output: bool = True,
     **kwargs
 ) -> float:
@@ -19,10 +20,13 @@ def gene_set_coexpression(
         annotated ``AnnData`` object with counts from spatial data
     seq_data : AnnData
         annotated ``AnnData`` object with counts scRNAseq data
-    overlap_threshold : int, optional
+    overlap_threshold : int, optional (Default: 5)
         minimal overlap of genes between a gene set and the common features bewteen
         the spatial and sequencing datasets for the gene set to be tested.
-    pipeline_output : bool, optional
+    min_cells : int, optional (Default: 20)
+        The number of cells a gene must be expressed in at minimum to contribute to
+        coexpression similarity score.
+    pipeline_output : bool, optional (Default: True)
         flag whether to output a single value (True) or results broken down into
         gene sets
 
@@ -44,9 +48,13 @@ def gene_set_coexpression(
     anns = op.requests.Annotations.get(resources='MSigDB')
     geneset_dict = _get_msigdb_collection(anns)
 
-    # Filter gene sets that can be tested
-    spatial_features = spatial_data.var_names.tolist()
-    seq_features = seq_data.var_names.tolist()
+    # Filter gene sets that can be tested (gene expressed in at least X cells)
+    vars_in_x_cells_st = sc.pp.filter_genes(spatial_data, min_cells = min_cells, inplace=False)[0]
+    spatial_features = spatial_data.var_names[vars_in_x_cells_st].tolist()
+
+    vars_in_x_cells_sc = sc.pp.filter_genes(seq_data, min_cells = min_cells, inplace=False)[0]
+    seq_features = seq_data.var_names[vars_in_x_cells_sc].tolist()
+
     common_features = list(set(spatial_features).intersection(seq_features))
 
     sets_to_remove = list()
@@ -115,6 +123,8 @@ def _mtx_subset_and_diff(
     mat_sc_sub[np.tril_indices(len(ids_in_set))] = np.nan
 
     mean_coexp_sc = np.nanmean(mat_sc_sub)
+    mean_coexp_st = np.nanmean(mat_st_sub)
+    print(f'Average geneset coexpression in spatial data: {mean_coexp_st}')
     print(f'Average geneset coexpression in seq data: {mean_coexp_sc}')
 
     # Absolute mean diff
