86f860efd352a9ec1c322bfb703c9736c99138a7
Habib Rehman <hrehma6@illinois.edu>
2022-10-06 23:41:09 -0500

    added stardist

--- txsim/preprocessing/__init__.py
+++ txsim/preprocessing/__init__.py
@@ -1,6 +1,6 @@
 from ._normalization import normalize_total, normalize_pearson_residuals, normalize_by_area
 from .normalization_sc import normalize_sc
-from ._segmentation import segment_nuclei, segment_cellpose, segment_binning
+from ._segmentation import segment_nuclei, segment_cellpose, segment_binning, segment_stardist
 from ._assignment import basic_assign, run_pciSeq
 from ._countgeneration import generate_adata, calculate_alpha_area
 from ._ctannotation import run_majority_voting, run_ssam
