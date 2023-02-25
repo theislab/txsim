1df0eebac9d1344808e5c267eaf1b41a865ac5a3
aslik97 <aslik97@hotmail.com>
2023-02-25 20:50:59 +0100

    Update _combined.py

--- txsim/metrics/_combined.py
+++ txsim/metrics/_combined.py
@@ -4,6 +4,8 @@ import pandas as pd
 import scanpy as sc
 import seaborn as sns
 import glob as glob
+import re as re
+import os as os
 from os import listdir
 from os.path import isfile, join
 from _coexpression_similarity import *
@@ -65,27 +67,42 @@ def all_metrics(
     return pd.DataFrame.from_dict(metrics, orient='index')
 
 adata_sc = sc.read_h5ad("/Users/aslihankullelioglu/Downloads/txsim/sc_normalized.h5ad")
-panel_to_celltype =  {'Stromal broad': 'Stromal broad',
-  'Fibroblasts + PVL': 'Stromal broad',
-  'B-cells': 'Immune broad',
-  'T-cells':'Immune broad',
-  'Myeloid': 'Immune broad',
-  'Immune broad': 'Immune broad',               
-  'Epithelial broadl': 'Epithelial broad',
-  'None': 'None'}
+# panel_to_celltype =  {'Stromal broad': 'Stromal broad',
+#   'Fibroblasts + PVL': 'Stromal broad',
+#   'B-cells': 'Immune broad',
+#   'T-cells':'Immune broad',
+#   'Myeloid': 'Immune broad',
+#   'Immune broad': 'Immune broad',               
+#   'Epithelial broadl': 'Epithelial broad',
+#   'None': 'None'}
+
+#spatial_data_path = "/Users/aslihankullelioglu/Downloads/txsim/spatial"
+adata_sp_baysor_list = glob.glob('/Users/aslihankullelioglu/Downloads/txsim/spatial/*.h5ad')
 df_list = []
-spatial_data_path = "/Users/aslihankullelioglu/Downloads/txsim/spatial"
-spatial_files = [f for f in listdir(spatial_data_path) if isfile(join(spatial_data_path,f))]
-for file in spatial_files[:5]:
-    if file != '.DS_Store':
-        curr_whole_path = spatial_data_path + '/' + file
-        curr_adata_sp = sc.read_h5ad(curr_whole_path)
-        sc.pp.filter_cells(curr_adata_sp, min_genes=1, inplace=True)
-        curr_adata_sp.obs = curr_adata_sp.obs.rename(columns={'celltype': 'celltype_panel'})
-        curr_adata_sp.obs['celltype'] = [(panel_to_celltype)[k] for k in curr_adata_sp.obs['celltype_panel']]
-        curr_adata_sp.obs['celltype'] = curr_adata_sp.obs['celltype'].astype('category')
-        output = all_metrics(curr_adata_sp,adata_sc)
-        df_list.append(output)
+for adata_sp_name in adata_sp_baysor_list[:5]:
+    adata_sp = sc.read_h5ad(adata_sp_name)
+    if re.match('baysor', adata_sp_name):
+        adata_sp = adata_sp[~np.isnan(adata_sp.layers['lognorm']).any(axis=1)]
+        adata_sp.write_h5ad("new_"+os.path.basename(adata_sp_name))
+        
+    #adata_sp.obs = adata_sp.obs.rename(columns={'celltype': 'celltype_panel'})
+    #adata_sp.obs['celltype'] = [(panel_to_celltype)[k] for k in adata_sp.obs['celltype_panel']]
+    #adata_sp.obs['celltype'] = adata_sp.obs['celltype'].astype('category')
+    output = all_metrics(adata_sp,adata_sc)
+    df_list.append(output)
+
+#spatial_files = [f for f in listdir(spatial_data_path) if isfile(join(spatial_data_path,f))]
+# for file in adata_sp_baysor_list[:5]:
+#     if file != '.DS_Store':
+#         curr_whole_path = spatial_data_path + '/' + file
+#         curr_adata_sp = sc.read_h5ad(curr_whole_path)
+#         sc.pp.filter_cells(curr_adata_sp, min_genes=1, inplace=True)
+#         curr_adata_sp.obs = curr_adata_sp.obs.rename(columns={'celltype': 'celltype_panel'})
+#         curr_adata_sp.obs['celltype'] = [(panel_to_celltype)[k] for k in curr_adata_sp.obs['celltype_panel']]
+#         curr_adata_sp.obs['celltype'] = curr_adata_sp.obs['celltype'].astype('category')
+#         output = all_metrics(curr_adata_sp,adata_sc)
+#         df_list.append(output)
+
 
 
 
