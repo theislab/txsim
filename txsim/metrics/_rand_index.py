6756ed0d6a45848114d782ce8c04f6b2e0a0c88a
Habib Rehman <hrehma6@illinois.edu>
2023-06-05 01:09:43 -0500

    adding annotation similarity matrix

--- txsim/metrics/_rand_index.py
+++ txsim/metrics/_rand_index.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 from pandas import DataFrame
 import sklearn.metrics
+from anndata import AnnData
 
 def calc_rand_index(assignments: DataFrame):
     rand_matrix = np.zeros([len(assignments.columns), len(assignments.columns)])
@@ -26,3 +27,33 @@ def aggregate_rand_index(matrices: list):
     df_std.loc[:,:] = np.std(np.array( matrices ), axis=0)
     
     return [df_mean, df_std]
+
+def calc_annotation_similarity(adata1: AnnData, adata2: AnnData):
+    #Lots of indexing- get all the spots assigned to a cell (in at least one of the adata objects)
+    #notnull = assigned to cell
+    #For these indices, get the cell_id
+    indices = adata2.uns['spots']['cell'][adata1.uns['spots']['cell'].notnull() | adata2.uns['spots']['cell'].notnull()]
+
+    #Given the cell_id, get the celltypes of the cell for each spot
+    cellann1 = adata1.obs[['cell_id','celltype']].set_index('cell_id').loc[indices.values,'celltype'].values
+    #Also for second adata
+    cellann2 = adata2.obs[['cell_id','celltype']].set_index('cell_id').loc[indices.values,'celltype'].values
+
+    #calc how many are the same
+    similarity = sum(cellann1.astype(str) == cellann2.astype(str)) / len(cellann2)
+
+    return similarity
+
+def calc_annotation_matrix(adata_list: list, name_list: list):
+    ann_matrix = np.zeros([len(adata_list), len(adata_list)])
+    ann_matrix = pd.DataFrame(ann_matrix)
+    for i in range(len(adata_list)):
+        for j in range(len(adata_list)):
+            adata1 = adata_list[i]
+            adata2 = adata_list[j]
+            ann_matrix.iloc[i, j] = calc_annotation_similarity(adata1, adata2)
+
+    ann_matrix.columns = name_list
+    ann_matrix.index = name_list
+    
+    return ann_matrix
