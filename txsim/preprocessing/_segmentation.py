76c7e60b3c384b5292716e596759bc4cae20e7a7
cw_liao <48869711+Dondonn@users.noreply.github.com>
2023-09-27 15:21:50 +0200

    Update _segmentation.py

--- txsim/preprocessing/_segmentation.py
+++ txsim/preprocessing/_segmentation.py
@@ -200,6 +200,285 @@ def segment_cellpose(
         
     return res
 
+def segment_watershed(
+    img: NDArrayA,
+    hyperparams: Optional[dict]
+        
+)-> NDArrayA:
+    
+    from skimage.segmentation import watershed
+    from scipy.ndimage import distance_transform_edt
+    from skimage.exposure import adjust_gamma, adjust_log, adjust_sigmoid, equalize_adapthist, equalize_hist, rescale_intensity
+    from skimage.morphology import remove_small_objects, label,remove_small_holes
+    from skimage.feature import peak_local_max
+    from skimage.filters import threshold_otsu, threshold_triangle, gaussian, median, rank
+    from skimage.morphology import (square, rectangle, diamond, disk, cube,
+                            octahedron, ball, octagon, star)
+    import skimage.filters.rank as rank
+    from scipy import ndimage as ndi
+    import matplotlib.pyplot as plt
+    import inspect
+
+    def filter_params(func, params):
+        """Return only those params that are in the function's signature."""
+        signature_params = set(inspect.signature(func).parameters)
+        return {k: v for k, v in params.items() if k in signature_params}
+
+    def visualize(image, segmentation_labels,img_name,display = False, *args, **kwargs):
+        # Display the original image
+        fig, axes = plt.subplots(figsize=(10, 10))
+        # Display the original image with the watershed segmentation overlaid
+        axes.imshow(image, cmap='gray') # Original image
+        axes.imshow(segmentation_labels, cmap='nipy_spectral', alpha=0.5) # Segmentation labels overlaid with transparency
+        axes.set_title("Watershed Over Original Image")
+        #write image to file
+        plt.savefig(img_name+'_watershed.png')
+        if display:
+            plt.show()
+
+
+    def find_local_maxima(distance_transform, min_distance=5):
+        local_maxima_coords = peak_local_max(distance_transform, min_distance=min_distance)
+        local_maxima = np.zeros_like(distance_transform, dtype=bool)
+        local_maxima[tuple(local_maxima_coords.T)] = True
+        return label(local_maxima)
+
+    # Define normalization function mappings
+    normalization_functions = {
+        "gamma": adjust_gamma,
+        "log": adjust_log,
+        "sigmoid": adjust_sigmoid,
+        # Add other mappings as needed
+    }
+
+    # Define contrast adjustment function mappings
+    contrast_adjustment_functions = {
+        "equalize_adapthist": equalize_adapthist,
+        "equalize_hist": equalize_hist,
+        "rescale_intensity": rescale_intensity,
+        # Add other mappings as needed
+    }
+
+    # Define blur function mappings
+    blur_functions = {
+        "gaussian": gaussian,
+        "median": median,
+        # Add other mappings as needed
+    }
+
+    # Define threshold function mappings
+    threshold_functions = {
+        "otsu": threshold_otsu,
+        "triangle": threshold_triangle,
+        "local_otsu": rank.otsu,
+        # Add other mappings as needed
+    }
+
+    # Define distance transform function mappings
+    distance_transform_functions = {
+        "distance_transform_edt": distance_transform_edt,
+        # Add other mappings as needed
+    }
+
+    # Define local maxima function mappings
+    local_maxima_functions = {
+        "find_local_maxima": find_local_maxima,
+        # Add other mappings as needed
+    }
+
+    # Define post-processing function mappings
+    post_processing_functions = {
+        "remove_small_objects": remove_small_objects,
+        "remove_small_holes": remove_small_holes,
+        # Add other mappings as needed
+    }
+
+    footprints = {
+        "square": square,
+        "diamond": diamond,
+        "disk": disk,
+        "cube": cube,
+        "octahedron": octahedron,
+        "ball": ball,
+        "star": star,
+        # Add other mappings as needed
+    }
+
+    threshold_apply = lambda  img: img > threshold_value
+
+    # Get parameters
+    if hyperparams is not None:
+        
+        normalize_func = normalization_functions.get(hyperparams.get("normalize_func","gamma"))
+        contrast_adjustment_func = contrast_adjustment_functions.get(hyperparams.get("contrast_adjustment_func","equalize_hist"))
+        blur_func = blur_functions.get(hyperparams.get("blur_func","gaussian"))
+        threshold_func = threshold_functions.get(hyperparams.get("threshold_func","otsu"))
+        distance_transform_func = distance_transform_functions.get(hyperparams.get("distance_transform_func","distance_transform_edt"))
+        local_maxima_func = local_maxima_functions.get(hyperparams.get("local_maxima_func","find_local_maxima"))
+        #post_processing_func = post_processing_functions.get(hyperparams.get("post_processing_func"))
+
+        # Normalize image
+        if normalize_func is not None:
+
+            normalize_params = {
+                "gamma": hyperparams.get("normalize_gamma", 1),
+                "gain": hyperparams.get("normalize_gain", 1),
+                "inv": hyperparams.get("normalize_inv", False),
+                "cutoff": hyperparams.get("normalize_cutoff", 0.5)
+            }
+            valid_params = filter_params(normalize_func, normalize_params)
+            img = normalize_func(img,**valid_params)
+
+        # Contrast adjustment
+        if contrast_adjustment_func is not None:
+            contrast_adjustment_params = {
+                "kernel_size": hyperparams.get("contrast_adjustment_kernel_size"),
+                "clip_limit": hyperparams.get("contrast_adjustment_clip_limit", 0.01),
+                "nbins": hyperparams.get("contrast_adjustment_nbins", 256),
+                "mask": hyperparams.get("contrast_adjustment_mask"),
+                "in_range": hyperparams.get("contrast_adjustment_in_range", "image"),
+                "out_range": hyperparams.get("contrast_adjustment_out_range", "dtype")
+            }
+
+            valid_params = filter_params(contrast_adjustment_func, contrast_adjustment_params)
+            img = contrast_adjustment_func(img, **valid_params)
+        # Blur image
+        if blur_func is not None:
+            blur_params = {
+                "sigma": hyperparams.get("blur_sigma", 1),
+                "output": hyperparams.get("blur_output"),
+                "mode": hyperparams.get("blur_mode", 'nearest'),
+                "cval": hyperparams.get("blur_cval", 0),
+                "preserve_range": hyperparams.get("blur_preserve_range", False),
+                "truncate": hyperparams.get("blur_truncate", 4.0)
+            }
+
+            valid_params = filter_params(blur_func, blur_params)
+            img = blur_func(img, **valid_params)
+
+        # Threshold (Masking), Apply thresholding to detect nuclei
+        if threshold_func is not None:
+
+            threshold_params = {
+                "nbins": hyperparams.get("threshold_nbins", 256),
+                "hist": hyperparams.get("threshold_hist"),
+                "out": hyperparams.get("threshold_out"),
+                "mask": hyperparams.get("threshold_mask"),
+                "shift_x": hyperparams.get("threshold_shift_x", False),
+                "shift_y": hyperparams.get("threshold_shift_y", False),
+                "shift_z": hyperparams.get("threshold_shift_z", False),
+                "footprint_func": hyperparams.get("threshold_footprint", "square"),
+                "footprint_size": hyperparams.get("threshold_footprint_size", 10)
+            }
+
+            if threshold_func == rank.otsu: # Check if the method is local Otsu
+                # Retrieve footprint or any other specific parameter for local Otsu
+                footprint_func = footprints.get(threshold_params.get("footprint_func", "square"))
+                # Footprint size 
+                footprint_size = threshold_params.get("footprint_size", 10)
+                # Create footprint
+                footprint = footprint_func(footprint_size)
+                # Here, use the local Otsu function as needed
+                threshold_params["footprint"] = footprint
+
+                valid_params = filter_params(threshold_func, threshold_params)
+                threshold_value = threshold_func(img,**valid_params)
+                #print(threshold_value)
+                nuclei = img >= threshold_value # Applying the lambda function
+                
+
+            else: # For global thresholding methods
+                valid_params = filter_params(threshold_func, threshold_params)
+                threshold_value = threshold_func(img, **valid_params)
+                nuclei = threshold_apply(img)
+
+                    # Remove small objects that are not nuclei
+
+            # Initialize an index to keep track of the number of post-processing steps
+            post_processing_index = 1
+
+            while True:
+                
+                
+                # Extract the function and parameters for the current post-processing step
+                post_processing_func_name = hyperparams.get(f"post_processing_func_{post_processing_index}")
+                post_processing_func = post_processing_functions.get(post_processing_func_name)
+                
+                if post_processing_func is None:
+                    break  # Exit the loop if there is no function for the current index
+
+                # Construct a dictionary of parameters for the current post-processing step
+                post_processing_params = {
+                    "min_size": hyperparams.get(f"post_processing_min_size_{post_processing_index}", 64),
+                    "area_threshold": hyperparams.get(f"post_processing_area_threshold_{post_processing_index}", 64),
+                    "connectivity": hyperparams.get(f"post_processing_connectivity_{post_processing_index}", 1),
+                    "out": hyperparams.get(f"post_processing_out_{post_processing_index}", None),
+                }
+
+                # Apply the post-processing function with the extracted parameters
+                valid_params = filter_params(post_processing_func, post_processing_params)
+                nuclei = post_processing_func(nuclei, **valid_params)
+                
+                # Increment the index to process the next post-processing step (if any)
+                post_processing_index += 1
+
+        # Perform distance transform
+        if distance_transform_func is not None:
+
+            distance_transform_params = {
+        "sampling": hyperparams.get("distance_transform_sampling"),
+        "return_distances": hyperparams.get("distance_transform_return_distances", True),
+        "return_indices": hyperparams.get("distance_transform_return_indices", False),
+        "distances": hyperparams.get("distance_transform_distances"),
+        "indices": hyperparams.get("distance_transform_indices")
+            }
+
+            valid_params = filter_params(distance_transform_func, distance_transform_params)
+            distance_transform = distance_transform_func(nuclei, **valid_params)
+
+        # Find local maxima to use as markers for watershed
+        if local_maxima_func is not None:
+            local_maxima_params = {
+        "min_distance": hyperparams.get("local_maxima_min_distance", 5)
+            }
+
+            local_maxima = local_maxima_func(distance_transform, **local_maxima_params)
+        # Perform watershed segmentation
+
+        segmentation_labels = watershed(-distance_transform, local_maxima, **hyperparams.get("watershed_params", {}),mask=nuclei, watershed_line=True)
+        
+        if "visualize" in hyperparams:
+            
+            visualize(img,segmentation_labels, hyperparams.get("visualize", "img"))
+    else:
+        min_distance = 10
+        gaussian_sigma = 1
+        min_size = 15
+
+        # Smooth image
+        img_smooth = gaussian(img, sigma=gaussian_sigma)
+    
+        # Get local maxima
+        #local_maxi = peak_local_max(img_smooth, min_distance=min_distance)
+    
+        # Get distance transform
+        distance = distance_transform_edt(img_smooth)
+
+        # Get markers
+        markers = find_local_maxima(distance)
+
+        # Get threshold
+        threshold_value = threshold_otsu(img_smooth)
+        nuclei = threshold_apply(img_smooth) # Applying the lambda function
+    
+        # Perform watershed segmentation
+        segmentation_labels = watershed(-distance, markers, mask=nuclei,  watershed_line=True)
+    
+        # Remove small objects
+        segmentation_labels = remove_small_objects(segmentation_labels, min_size=min_size)
+
+    return segmentation_labels
+
 def segment_binning(
     img: NDArrayA,
     bin_size: int
