09c59241b3441b7983b76d4d47ca351d351f252a
Liya Zaygerman <liya.zaygerman@tum.de>
2023-11-08 14:44:29 +0100

    initial commit for jensen-shannon

---
+++ txsim/metrics/_jensen-shannon_divergence.py
@@ -0,0 +1,53 @@
+import scanpy as sc
+import numpy as np
+import pandas as pd
+from anndata import AnnData
+from scipy.sparse import issparse
+
+def jensen_shannon_divergence(adata_sp: AnnData, adata_sc: AnnData, 
+                              key:str='celltype', layer:str='lognorm', 
+                              pipeline_output: bool=True):
+    """Calculate the Jensen-Shannon divergence between the two distributions:
+    the spatial and dissociated single-cell data distributions. 
+    ----------
+    adata_sp: AnnData
+        annotated ``AnnData`` object containing the spatial single-cell data
+    adata_sc: AnnData
+        annotated ``AnnData`` object containing the dissociated single-cell data
+    key: str (default: 'celltype')
+         .obs column of ``AnnData`` that contains celltype information
+    layer: str (default: 'lognorm')
+         layer of ```AnnData`` to use to compute the metric
+    pipeline_output: bool (default: True)
+
+    Returns
+    -------
+    overall_metric: float
+        overall Jensen-Shannon divergence between the two distributions
+    per_gene_metric: float
+        per gene Jensen-Shannon divergence between the two distributions
+    per_celltype_metric: float
+        per celltype Jensen-Shannon divergence between the two distributions
+    """
+
+    ### SET UP 
+    # set the .X layer of each of the adatas to be log-normalized counts
+    adata_sp.X = adata_sp.layers[layer]
+    adata_sc.X = adata_sc.layers[layer]
+
+    # take the intersection of genes present in adata_sp and adata_sc, as a list
+    intersect = list(set(adata_sp.var_names).intersection(set(adata_sc.var_names)))
+
+    # subset adata_sc and adata_sp to only include genes in the intersection of adata_sp and adata_sc 
+    adata_sc=adata_sc[:,intersect]
+    adata_sp=adata_sp[:,intersect]
+
+    ##############################
+    #actual calculation of metric#
+
+    ##############################
+
+    if pipeline_output:
+         return overall_metric
+
+    return overall_metric, per_gene_metric, per_celltype_metric
