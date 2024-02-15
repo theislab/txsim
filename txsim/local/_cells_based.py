260e329e9a02ec4b05fda0d61cae977f85f1bfd6
benklerc <159771908+benklerc@users.noreply.github.com>
2024-02-15 15:36:48 +0100

    Added  get_number_celltypes method.py

--- txsim/local/_cells_based.py
+++ txsim/local/_cells_based.py
@@ -39,3 +39,42 @@ def _get_cell_density_grid(
     return H
 
 
+def _get_number_of_celltypes(
+        adata_sp: ad.AnnData,
+        bins: Tuple[int],
+        cells_x_col: str = "x",
+        cells_y_col: str = "y",)-> np.ndarray:
+    """Get number of celltypes
+    
+    Parameters
+    ---------
+    adata_sp: AnnData
+        Annotated AnnData object containing spatial transcriptomics data.
+    region_range : Tuple[Tuple[float, float], Tuple[float, float]]
+        The range of the grid specified as ((y_min, y_max), (x_min, x_max)).
+    image : NDArray
+        read from image of dapi stained cell-nuclei
+    n_bins : List[int], optional
+        The number of bins along the y and x axes, formatted as [ny, nx]. 
+        Use either `bin_width` or `n_bins` to define grid cells.
+        
+    Returns
+    -------
+    A : np.ndarray of floats
+        number of celltypes per bin
+        one array per cell type, one array per x coordinate
+        A[celltype][x coordinate][y coordinate]
+    """
+    
+    #necessary to check if region is valid?
+    # name of celltypes in adata all the same?
+    
+    celltypes = pd.Index(adata.obs["louvain"].unique())
+    print(bins)
+    A = np.zeros((len(celltypes),bins[0]+1,bins[1]+1)) # one dataframe for each celltype, with |x| lists of |y| variables. variables number of cell type(specific to
+        #dataframe and bin
+    
+    for index, row in adata_sp.obs.iterrows():
+        A[celltypes.get_loc(row['louvain']),row[cells_x_col],row[cells_y_col]] += 1
+    
+    return A
