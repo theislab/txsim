357525ede31625eb9fc63730d38b390ebc944992
kreuzerm <kreuzerm@cip.ifi.lmu.de>
2024-02-16 15:53:45 +0100

    update 16.02.24

--- txsim/local/__init__.py
+++ txsim/local/__init__.py
@@ -1,3 +1,3 @@
 from ._analysis_wrappers import cell_and_spot_statistics, metrics
-from ._cells_based import _get_cell_density_grid #TODO: remove this, after dev
+from ._cells_based import _get_cell_density_grid, _major_celltype_perc, _get_celltype_density #TODO: remove this, after dev
 from ._metrics import _get_knn_mixing_grid #TODO: remove this, after dev
