c1cfd6a093774be84ada5e8923e5d0568ec17bad
schaeferti <159773380+schaeferti@users.noreply.github.com>
2024-02-16 15:48:12 +0100

    Update _cells_based.py (celltype density)

--- txsim/local/_cells_based.py
+++ txsim/local/_cells_based.py
@@ -38,42 +38,40 @@ def _get_cell_density_grid(
     H = np.histogram2d(df_cells[cells_y_col], df_cells[cells_x_col], bins=bins, range=region_range)[0]
     return H
 
-def get_celltype_density(adata_sp: AnnData, celltype: str, x_min: int, x_max: int, y_min: int, y_max: int, bins):
+def get_celltype_density(adata_sp: ad, region_range: Tuple[Tuple[float, float], Tuple[float, float]], bins: Tuple[int, int], celltype, obs_key: str = "celltype",cells_x_col: str = "x",
+    cells_y_col: str = "y",)-> np.ndarray:
     """Get celltype density.
 
     Parameters
     ----------
     adata_sp: AnnData
         Annotated ``AnnData`` object with counts from spatial data
-    celltype: str
-        celltype
-    x_min : int, x_max : int, y_min : int, y_max : int 
-        crop coordinates
-
-    bins : int or array_like or [int, int] or [array, array]
-        The bin specification:
-        If int, the number of bins for the two dimensions (nx=ny=bins).
-        If array_like, the bin edges for the two dimensions (x_edges=y_edges=bins).
-        If [int, int], the number of bins in each dimension (nx, ny = bins).
-        If [array, array], the bin edges in each dimension (x_edges, y_edges = bins).
-        A combination [int, array] or [array, int], where int is the number of bins and array is the bin edges.
+    region_range : Tuple[Tuple[float, float], Tuple[float, float]]
+        The range of the grid specified as ((y_min, y_max), (x_min, x_max)).
+    bins : Tuple[int, int]
+        The number of bins along the y and x axes, formatted as (ny, nx).
+    obs_key : str, default "celltype"
+        The column name in adata_sp.obs and adata_sc.obs for the cell type annotations.
+    cells_x_col : str, default "x"
+        The column name in adata_sp.obs for the x-coordinates of cells.
+    cells_y_col : str, default "y"
+        The column name in adata_sp.obs for the y-coordinates of cells.
     Returns
     -------
     H : array of floats
         density of celltype per bin
-    range : range of binning 
+    range : range of binning
     """
     df =  adata_sp.obs
-    range = (x_min,x_max,y_min,y_max)
-    
-    H_total = np.histogram2d(df['x'],df['y'], bins, range)[0]
 
-    df = df.loc[df["louvain"]==celltype]
+    H_total = np.histogram2d(df[cells_y_col],df[cells_x_col], bins=bins, range=region_range)[0]
 
-    H_celltype = np.histogram2d(df['x'],df['y'], bins, range)[0]
+    df = df.loc[df[obs_key]==celltype]
+
+    H_celltype = np.histogram2d(df[cells_y_col], df[cells_x_col], bins=bins, range=region_range)[0]
 
     H = H_celltype/H_total
-    H[np.isnan(H)] = 0     
+    H[np.isnan(H)] = 0
     H = H.T
-    
+
     return H, range
