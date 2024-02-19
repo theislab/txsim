add70d38f2bc0b8e55940cffa550d033faa99fc4
benklerc <159771908+benklerc@users.noreply.github.com>
2024-02-19 15:27:16 +0100

    Update _cells_based.py updated parameters

--- txsim/local/_cells_based.py
+++ txsim/local/_cells_based.py
@@ -41,9 +41,12 @@ def _get_cell_density_grid(
 
 def _get_number_of_celltypes(
         adata_sp: ad.AnnData,
+        region_range: Tuple[Tuple[float, float], Tuple[float, float]],
         bins: Tuple[int],
         cells_x_col: str = "x",
-        cells_y_col: str = "y",)-> np.ndarray:
+        cells_y_col: str = "y",
+        obs_key: str = "celltype",
+):
     """Get number of celltypes
     
     Parameters
@@ -52,18 +55,21 @@ def _get_number_of_celltypes(
         Annotated AnnData object containing spatial transcriptomics data.
     region_range : Tuple[Tuple[float, float], Tuple[float, float]]
         The range of the grid specified as ((y_min, y_max), (x_min, x_max)).
-    image : NDArray
-        read from image of dapi stained cell-nuclei
     n_bins : List[int], optional
         The number of bins along the y and x axes, formatted as [ny, nx]. 
         Use either `bin_width` or `n_bins` to define grid cells.
-        
+    cells_x_col : str, default "x"
+        The column name in adata.obs for the x-coordinates of cells.
+    cells_y_col : str, default "y"
+        The column name in adata.obs for the y-coordinates of cells.
+    obs_key : str, default "celltype"
+        The column name in adata_sp.obs and adata_sc.obs for the cell type annotations.
     Returns
     -------
     H :  np.ndarray
         A 2D numpy array representing the number of celltypes in each grid bin.
     """
+    
     df_cells = adata_sp.obs[[cells_x_col, cells_y_col, obs_key]].drop_duplicates()
-    H = np.histogram2d(df_cells[cells_y_col], df_cells[cells_x_col], bins=bins, range=region_range)[0]
-            
+    H = np.histogram2d(df_cells[cells_y_col], df_cells[cells_x_col], bins=bins, range = region_range)[0]
     return H
