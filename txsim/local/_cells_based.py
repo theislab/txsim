82779418d15b0f0583c7ede15016dd68a5928c06
LouisK92 <37270609+LouisK92@users.noreply.github.com>
2024-02-22 13:13:12 +0100

    Clean up docstrings, add celltype ratio metric, wrap fcts

--- txsim/local/_cells_based.py
+++ txsim/local/_cells_based.py
@@ -1,6 +1,6 @@
 import numpy as np
 import anndata as ad
-from typing import Tuple
+from typing import Tuple, Dict
 
 #TODO (fcts: _get_<...>_grid): 
 # "celltype_density", "number_of_celltypes", "major_celltype_perc", "summed_cell_area", "spot_uniformity_within_cells"
@@ -38,29 +38,82 @@ def _get_cell_density_grid(
     H = np.histogram2d(df_cells[cells_y_col], df_cells[cells_x_col], bins=bins, range=region_range)[0]
     return H
 
-def get_celltype_density(adata_sp: ad, region_range: Tuple[Tuple[float, float], Tuple[float, float]], bins: Tuple[int, int], obs_key: str = "celltype",cells_x_col: str = "x",
-    cells_y_col: str = "y",):
-    """Get celltype density.
+def _get_cell_density_grid_per_celltype(
+    adata_sp: ad.AnnData, 
+    region_range: Tuple[Tuple[float, float], Tuple[float, float]], 
+    bins: Tuple[int, int], 
+    obs_key: str = "celltype",
+    cells_x_col: str = "x",
+    cells_y_col: str = "y"
+) -> Dict[str, np.ndarray]:
+    """Calculate the density of cells within each grid bin for each cell type.
 
     Parameters
     ----------
     adata_sp: AnnData
-        Annotated ``AnnData`` object with counts from spatial data
+        Annotated ``AnnData`` object with counts from spatial data.
     region_range : Tuple[Tuple[float, float], Tuple[float, float]]
         The range of the grid specified as ((y_min, y_max), (x_min, x_max)).
     bins : Tuple[int, int]
         The number of bins along the y and x axes, formatted as (ny, nx).
     obs_key : str, default "celltype"
-        The column name in adata_sp.obs and adata_sc.obs for the cell type annotations.
+        The column name in adata_sp.obs for the cell type annotations.
     cells_x_col : str, default "x"
         The column name in adata_sp.obs for the x-coordinates of cells.
     cells_y_col : str, default "y"
         The column name in adata_sp.obs for the y-coordinates of cells.
+        
     Returns
     -------
-    H_dict :  dictionary with celltyp as key
-              array with density per bin as value
-    range : range of binning
+    dict of np.ndarrays
+        key: celltype 
+        value: 2D numpy array representing the cell density of the given cell type in each grid bin.
+
+    """
+    df =  adata_sp.obs
+
+    celltypes = df[obs_key].unique()
+
+    H_dict = {}
+    for celltype in celltypes:
+      df_filtered = df.loc[df[obs_key]==celltype]
+      H_celltype = np.histogram2d(df_filtered[cells_y_col], df_filtered[cells_x_col], bins=bins, range=region_range)[0]
+      H_dict[celltype] = H_celltype
+
+    return H_dict
+
+
+def _get_celltype_ratio_grid(
+    adata_sp: ad.AnnData, 
+    region_range: Tuple[Tuple[float, float], Tuple[float, float]], 
+    bins: Tuple[int, int], 
+    obs_key: str = "celltype",
+    cells_x_col: str = "x",
+    cells_y_col: str = "y"
+) -> Dict[str, np.ndarray]:
+    """Calculate the cell type ratio within each grid bin for each cell type.
+
+    Parameters
+    ----------
+    adata_sp: AnnData
+        Annotated ``AnnData`` object with counts from spatial data.
+    region_range : Tuple[Tuple[float, float], Tuple[float, float]]
+        The range of the grid specified as ((y_min, y_max), (x_min, x_max)).
+    bins : Tuple[int, int]
+        The number of bins along the y and x axes, formatted as (ny, nx).
+    obs_key : str, default "celltype"
+        The column name in adata_sp.obs for the cell type annotations.
+    cells_x_col : str, default "x"
+        The column name in adata_sp.obs for the x-coordinates of cells.
+    cells_y_col : str, default "y"
+        The column name in adata_sp.obs for the y-coordinates of cells.
+        
+    Returns
+    -------
+    dict of np.ndarrays
+        key: celltype 
+        value: 2D numpy array representing the cell type ratio of the given cell type in each grid bin.
+
     """
     df =  adata_sp.obs
 
@@ -76,5 +129,4 @@ def get_celltype_density(adata_sp: ad, region_range: Tuple[Tuple[float, float], 
       H[np.isnan(H)] = 0
       H_dict[celltype] = H
 
-
-    return H_dict, range
+    return H_dict
