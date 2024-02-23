4d81ba33301dfbef431146595c1f99160c0661c7
benklerc <159771908+benklerc@users.noreply.github.com>
2024-02-23 10:04:46 +0100

    Update _cells_based.py
    added new _get_number_of_celltypes using the _get_cell_density_gird_per_celltype

--- txsim/local/_cells_based.py
+++ txsim/local/_cells_based.py
@@ -39,3 +39,40 @@ def _get_cell_density_grid(
     return H
 
 
+def _get_number_of_celltypes(
+        adata_sp: ad.AnnData,
+        region_range: Tuple[Tuple[float, float], Tuple[float, float]],
+        bins: Tuple[int, int],
+        obs_key: str = "celltype",
+):
+    """Get number of celltypes
+
+    Parameters
+    ---------
+    adata_sp: AnnData
+        Annotated AnnData object containing spatial transcriptomics data.
+    region_range : Tuple[Tuple[float, float], Tuple[float, float]]
+        The range of the grid specified as ((y_min, y_max), (x_min, x_max)).
+    n_bins : List[int], optional
+        The number of bins along the y and x axes, formatted as [ny, nx].
+        Use either `bin_width` or `n_bins` to define grid cells.
+    obs_key : str, default "celltype"
+        The column name in adata_sp.obs and adata_sc.obs for the cell type annotations.
+    Returns
+    -------
+    array2d :  np.ndarray
+        A 2D numpy array representing the number of celltypes in each grid bin.
+    """
+    # calculate the cell density for every celltype
+    density_per_celltype = _get_cell_density_grid_per_celltype(adata_sp, region_range, bins, obs_key)
+    # stack density per celltype to a 3D array, 1st D: y bins, 2nd D: x bins, 3rd D: vector of length = number of celltypes
+    histograms = list(density_per_celltype.values())
+    histograms_3d = np.dstack(histograms)
+    # check for every celltype if their density >0
+    for i in range(len(histograms)):
+        mask = histograms_3d[:, :, i] > 0
+        histograms_3d[:, :, i][mask] = 1
+
+    # sum up vectors of 3rd Dimension
+    array2d = np.sum(histograms_3d, axis=2)
+    return array2d
