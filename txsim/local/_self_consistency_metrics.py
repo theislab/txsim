e03fc037db733a69efa34dcdb1d57bcd32fc28fa
benklerc <159771908+benklerc@users.noreply.github.com>
2024-02-23 09:00:07 +0100

    Update _self_consistency_metrics.py
    added the bin calculation to calc_adj_rand_index

--- txsim/local/_self_consistency_metrics.py
+++ txsim/local/_self_consistency_metrics.py
@@ -18,10 +18,14 @@ def calc_adj_rand_index(
         Annotated ``AnnData`` object with counts from spatial data and spots from clustering1
     adata_sp2 : AnnData
         Annotated ``AnnData`` object with counts from spatial data and spots from clustering2
+    region_range : Tuple[Tuple[float, float], Tuple[float, float]]
+        The range of the grid specified as ((y_min, y_max), (x_min, x_max)).
+    bins : Tuple[int, int]
+        The number of bins along the y and x axes, formatted as (ny, nx).
     uns_key : str
         Key where to find the data containing the spots information in both adata.uns
     ann_key : str
-        Key where the annotation for teh cell IDs are found in adata.uns[uns_key]
+        Key where the annotation for the cell IDs are found in adata.uns[uns_key]
     cells_x_col : str, default "x"
         The column name in adata_sp.obs for the x-coordinates of cells.
     cells_y_col : str, default "y"
@@ -39,17 +43,23 @@ def calc_adj_rand_index(
                                      ((adata_sp1.uns[uns_key][cells_x_col] >= region_range[1][0])&(adata_sp1.uns[uns_key][cells_x_col] <= region_range[1][1]))]
         df2 = adata_sp2.uns[uns_key][((adata_sp1.uns[uns_key][cells_y_col] >= region_range[0][0])&(adata_sp1.uns[uns_key][cells_y_col] <= region_range[0][1]))&
                                      ((adata_sp1.uns[uns_key][cells_x_col] >= region_range[1][0])&(adata_sp1.uns[uns_key][cells_x_col] <= region_range[1][1]))]
+        # assign bin to each xy coordinate and save in adata.uns
+        adata_sp1.uns[uns_key]["bin_y"] = pd.cut(adata_sp1.uns[uns_key][cells_y_col], bins=bins[0], labels=False)
+        adata_sp1.uns[uns_key]["bin_x"] = pd.cut(adata_sp1.uns[uns_key][cells_x_col], bins=bins[1], labels=False)
+        adata_sp2.uns[uns_key]["bin_y"] = pd.cut(adata_sp2.uns[uns_key][cells_y_col], bins=bins[0], labels=False)
+        adata_sp2.uns[uns_key]["bin_x"] = pd.cut(adata_sp2.uns[uns_key][cells_x_col], bins=bins[1], labels=False)
+        df1 = adata_sp1.uns[uns_key]
+        df2 = adata_sp2.uns[uns_key]
         assert (len(df1)==len(df2)), "AnnData Objects do not have the same number of genes in the specified region."
         # group by bins
         df1 = df1.groupby([cells_y_col, cells_x_col])[ann_key]
         df2 = df2.groupby([cells_y_col, cells_x_col])[ann_key]
         # new Dataframe, size as bins
-        ARI_per_bins = np.ndarray(bins, float)
+        ARI_per_bins = np.zeros(bins, float)
 
         # calculate ARI for every bin
-        # store ARI in new Dataframe
         for (y, x), group1 in df1:
-            group2 = df2.get_group((y, x))#, default=pd.DataFrame()
+            group2 = df2.get_group((y, x))
             c1 = group1.values
             c2 = group2.values
             ARI_per_bins[y, x] = sklearn.metrics.adjusted_rand_score(c1, c2)
