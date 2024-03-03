Fixed normalization for gene number

txsim/local/_metrics.py
@@ -285,8 +285,8 @@ def _get_relative_expression_similarity_across_genes_grid(
             abs_diff_sum_sp = np.sum(abs_diff_sp, axis=(0, 1))
 
             # calculate normalization factor
-            norm_factor_sc = mean_celltype_sc.shape[1] ** 2 * abs_diff_sum_sc
-            norm_factor_sp = mean_celltype_sc.shape[1] ** 2 * abs_diff_sum_sp
+            norm_factor_sc = (1/(mean_celltype_sc.shape[1] ** 2)) * abs_diff_sum_sc
+            norm_factor_sp = (1/(mean_celltype_sc.shape[1] ** 2)) * abs_diff_sum_sp
 
             # perform normalization
             # exclude the ones with norm_factor_sc, norm_factor_sp with zero