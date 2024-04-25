1f8d8838610997d65facb4a074f09bd89ddc2844
Liya Zaygerman <liya.zaygerman@tum.de>
2024-04-25 15:38:51 +0200

    changed fixed column names into variables

--- txsim/metrics/_main_inter_diff_expression_correlation.py
+++ txsim/metrics/_main_inter_diff_expression_correlation.py
@@ -32,6 +32,10 @@ def global_MIDEC(adata_sp_source : ad.AnnData,
                                 lower_threshold : float = 0.25,
                                 source_segmentation_name : str = 'source',
                                 target_segmentation_name : str = 'target',
+                                spots_x_col: str = 'x',
+                                spots_y_col: str = 'y',
+                                cell_col_name: str = 'cell',
+                                gene_col_name: str = 'Gene',
                                 which_metric = 'pearson'):
     """
     Main Intersection over Difference Expression Correlation (MIDEC).
@@ -66,13 +70,20 @@ def global_MIDEC(adata_sp_source : ad.AnnData,
     source_segmentation_name : str
         Name of the column with the source segmentation. Default is 'source'.
     target_segmentation_name : str
-        Name of the column with the target segmentation. Default is 'target'.
+        Name of the column with the target segmentation. Default is 'target'
+    spots_x_col : str
+        Name of the column with the x coordinates of the spots. Default is 'x'.
+    spots_y_col : str
+        Name of the column with the y coordinates of the spots. Default is 'y'.
+    which_metric : str
+        Name of the metric to be used. Default is 'pearson'. Other options are 
+        'spearman', 'MAE' (mean absolute error), and 'MSE' (mean square error). 
     
     Output
     -------
-    correlation : float
-        Pearson correlation coefficient between the intersection gene expression
-        vector and the difference gene expression vector.
+    metric : float
+        Metric value, correllation for 'pearson' and 'spearman', or error for
+        'MAE' and 'MSE'.
     """
     # if the thresholds make no sense - throw an error
     if upper_threshold >= 1:
@@ -81,14 +92,18 @@ def global_MIDEC(adata_sp_source : ad.AnnData,
         raise ValueError('upper_threshold must be > lower_threshold')
     elif lower_threshold <= 0:
         raise ValueError('lower_threshold must be > 0')
+    
+    # assert that one of the metrics is chosen
+    assert which_metric in ['pearson', 'spearman', 'MAE', 'MSE'], 'Invalid metric'
 
+    # get the spots from both AnnData objects
     source_spots = adata_sp_source.uns['spots'].copy()
     target_spots = adata_sp_target.uns['spots'].copy()
 
-    # rename the column 'cell' to source_segmentation_name and make it integer type
-    spots_df = source_spots.rename(columns={'cell': source_segmentation_name}).copy()
-    # add the column 'cell' from target segmentation to the spots_df
-    spots_df[target_segmentation_name] = target_spots['cell'].copy()
+    # rename the cell column to source_segmentation_name and make it integer type
+    spots_df = source_spots.rename(columns={cell_col_name: source_segmentation_name}).copy()
+    # add the cell column from target segmentation to the spots_df
+    spots_df[target_segmentation_name] = target_spots[cell_col_name].copy()
     # fill the NaN values with 0 and make the column integer type
     spots_df[source_segmentation_name] = spots_df[source_segmentation_name].fillna(0).astype(int)
     spots_df[target_segmentation_name] = spots_df[target_segmentation_name].fillna(0).astype(int)
@@ -100,6 +115,9 @@ def global_MIDEC(adata_sp_source : ad.AnnData,
                                 lower_threshold=lower_threshold,
                                 source_segmentation_name=source_segmentation_name,
                                 target_segmentation_name=target_segmentation_name,
+                                spots_x_col=spots_x_col,
+                                spots_y_col=spots_y_col,
+                                gene_col_name=gene_col_name,
                                 which_metric=which_metric)
 
     return metric
@@ -107,18 +125,22 @@ def global_MIDEC(adata_sp_source : ad.AnnData,
 def local_MIDEC(adata_sp_source : ad.AnnData,
                 adata_sp_target : ad.AnnData,
                 image : np.ndarray,
-                                x_min : int,
-                                x_max : int,
-                                y_min : int,
-                                y_max : int,
-                                min_number_of_spots_per_cell = 10,
-                                min_number_of_cells = 10,
-                                upper_threshold = 0.75,
-                                lower_threshold = 0.25,
-                                source_segmentation_name='source',
-                                target_segmentation_name = 'target',
-                                bins = 10,
-                                which_metric = 'pearson'):
+                x_min : int,
+                x_max : int,
+                y_min : int,
+                y_max : int,
+                min_number_of_spots_per_cell = 10,
+                min_number_of_cells = 10,
+                upper_threshold = 0.75,
+                lower_threshold = 0.25,
+                source_segmentation_name='source',
+                target_segmentation_name = 'target',
+                spots_x_col='x',
+                spots_y_col='y',
+                cell_col_name='cell',
+                gene_col_name='Gene',
+                bins = 10,
+                which_metric = 'pearson'):
     """
     Local Main Intersection over Difference Expression Correlation (MIDEC).
 
@@ -182,9 +204,9 @@ def local_MIDEC(adata_sp_source : ad.AnnData,
     source_spots = adata_sp_source.uns['spots']
     target_spots = adata_sp_target.uns['spots']
     # rename the column 'cell' to source_segmentation_name and make it integer type
-    spots_df = source_spots.rename(columns={'cell': source_segmentation_name}).copy()
+    spots_df = source_spots.rename(columns={cell_col_name: source_segmentation_name}).copy()
     # add the column 'cell' from target segmentation to the spots_df
-    spots_df[target_segmentation_name] = target_spots['cell'].copy()
+    spots_df[target_segmentation_name] = target_spots[cell_col_name].copy()
     # fill the NaN values with 0 and make the column integer type
     spots_df[source_segmentation_name] = spots_df[source_segmentation_name].fillna(0).astype(int)
     spots_df[target_segmentation_name] = spots_df[target_segmentation_name].fillna(0).astype(int)
@@ -210,14 +232,14 @@ def local_MIDEC(adata_sp_source : ad.AnnData,
             print(f"crop: {x_start}:{x_end}, {y_start}:{y_end}")
             
             # crop the spots_df
-            spots_crop = spots_df[(spots_df['x'].astype(int) >= x_start) &
-                                     (spots_df['x'].astype(int) < x_end) &
-                                     (spots_df['y'].astype(int) >= y_start) &
-                                     (spots_df['y'].astype(int) < y_end)].copy()
+            spots_crop = spots_df[(spots_df[spots_x_col].astype(int) >= x_start) &
+                                     (spots_df[spots_x_col].astype(int) < x_end) &
+                                     (spots_df[spots_y_col].astype(int) >= y_start) &
+                                     (spots_df[spots_y_col].astype(int) < y_end)].copy()
             
             # adjust the coordinates of the cropped dataframe
-            spots_crop['x'] = spots_crop['x'] - x_start
-            spots_crop['y'] = spots_crop['y'] - y_start
+            spots_crop[spots_x_col] = spots_crop[spots_x_col] - x_start
+            spots_crop[spots_y_col] = spots_crop[spots_y_col] - y_start
             spots_crop = spots_crop.reset_index(drop=True)
             
             # calculate the metric for the crop
@@ -228,6 +250,9 @@ def local_MIDEC(adata_sp_source : ad.AnnData,
                                                                             lower_threshold=lower_threshold,
                                                                             source_segmentation_name=source_segmentation_name,
                                                                             target_segmentation_name=target_segmentation_name,
+                                                                            spots_x_col=spots_x_col,
+                                                                            spots_y_col=spots_y_col,
+                                                                            gene_col_name=gene_col_name,
                                                                             which_metric=which_metric)
             i += 1
         j += 1 
@@ -236,13 +261,16 @@ def local_MIDEC(adata_sp_source : ad.AnnData,
 
 # Helper functions
 def main_inter_diff_expression_correlation(spots_df : pd.DataFrame,
-                                min_number_of_spots_per_cell : int = 10,
-                                min_number_of_cells : int = 10,
-                                upper_threshold : float = 0.75,
-                                lower_threshold : float = 0.25,
-                                source_segmentation_name : str = 'source',
-                                target_segmentation_name : str = 'target',
-                                which_metric : str = 'pearson'):
+                                min_number_of_spots_per_cell : int,
+                                min_number_of_cells : int,
+                                upper_threshold : float,
+                                lower_threshold : float,
+                                source_segmentation_name : str,
+                                target_segmentation_name : str,
+                                spots_x_col: str,
+                                spots_y_col: str,
+                                gene_col_name: str,
+                                which_metric : str):
     """
     Main Intersection over Difference Expression Correlation (MIDEC).
 
@@ -284,7 +312,7 @@ def main_inter_diff_expression_correlation(spots_df : pd.DataFrame,
         Pearson correlation coefficient between the intersection gene expression
         vector and the difference gene expression vector.
     """
-    spots_df = allocate_spots_to_pixels(spots_df)
+    spots_df = allocate_spots_to_pixels(spots_df, spots_x_col=spots_x_col, spots_y_col=spots_y_col)
 
     # get the ids from the source segmentation
     source_segmentation_ids = np.unique(spots_df[source_segmentation_name])
@@ -306,7 +334,8 @@ def main_inter_diff_expression_correlation(spots_df : pd.DataFrame,
                                                                         upper_threshold=upper_threshold,
                                                                         lower_threshold=lower_threshold,
                                                                         source_column_name=source_segmentation_name,
-                                                                        target_column_name=target_segmentation_name)
+                                                                        target_column_name=target_segmentation_name,
+                                                                        gene_col_name=gene_col_name)
         
         # if there is no overlap or no rest, continue with the next cell
         if main_overlap is None or rest is None:
@@ -319,7 +348,7 @@ def main_inter_diff_expression_correlation(spots_df : pd.DataFrame,
         elif which_metric == 'spearman':
             spearman_correlation = spearmanr(main_overlap, rest)[0]
             values.append(spearman_correlation)
-        elif which_metric == 'abs_error':
+        elif which_metric == 'MAE':
             main_overlap_normalized = main_overlap/np.linalg.norm(main_overlap)
             rest_normalized = rest/np.linalg.norm(rest)
             abs_error = np.abs(np.subtract(main_overlap_normalized, rest_normalized)).mean()
@@ -337,26 +366,25 @@ def main_inter_diff_expression_correlation(spots_df : pd.DataFrame,
         metric = None
     else:
         metric = np.mean(values)
-    #print(f"{len(values)} cells were considered for the calculation.")
 
     return metric
 
-def allocate_spots_to_pixels(spots_df):
+def allocate_spots_to_pixels(spots_df, spots_x_col, spots_y_col):
     """
     Allocate spots to pixels.
     
     Input
     ----------
     spots_df : pandas.DataFrame
-        DataFrame with columns 'x', 'y' and 'Gene'.
+        DataFrame with columns spots_x_col and spots_y_col
     
     Output
     -------
     spots_df_copy : pandas.DataFrame
-        DataFrame with columns 'x', 'y', 'Gene', 'pixel_x' and 'pixel_y'.
+        DataFrame with columns 'pixel_x' and 'pixel_y', spots_x_col and spots_y_col
     """
-    spots_df['pixel_x'] = spots_df['x'].astype(int)
-    spots_df['pixel_y'] = spots_df['y'].astype(int)
+    spots_df['pixel_x'] = spots_df[spots_x_col].astype(int)
+    spots_df['pixel_y'] = spots_df[spots_y_col].astype(int)
     return spots_df
 
 def add_segmentation_to_spots_df(spots_df : pd.DataFrame, 
@@ -368,14 +396,14 @@ def add_segmentation_to_spots_df(spots_df : pd.DataFrame,
     Input
     ----------
     spots_df : pandas.DataFrame
-        DataFrame with columns 'x', 'y', 'Gene', 'pixel_x' and 'pixel_y'.
+        DataFrame with columns 'pixel_x','pixel_y'
     segmentation : numpy.ndarray
         Segmentation array
     
     Output
     -------
     spots_df : pandas.DataFrame
-        DataFrame with columns 'x', 'y', 'Gene', 'pixel_x', 'pixel_y' and 'segmentation'.
+        DataFrame with columns 'pixel_x','pixel_y', segmentation_name
     """
     spots_df[segmentation_name] = segmentation[spots_df['pixel_y'], spots_df['pixel_x']]
     return spots_df
@@ -383,10 +411,11 @@ def add_segmentation_to_spots_df(spots_df : pd.DataFrame,
 
 def calculate_main_overlap_and_rest_one_cell(cell_id_source : int,
                                     spots_df : pd.DataFrame,
-                                    upper_threshold : float = 0.75,
-                                    lower_threshold : float = 0.25,
-                                    source_column_name : str = 'source',
-                                    target_column_name : str = 'target'):
+                                    upper_threshold : float,
+                                    lower_threshold : float,
+                                    source_column_name : str,
+                                    target_column_name : str,
+                                    gene_col_name : str):
     """
     Calculate the gene expression vectors for the main overlap (maximum overlap)
     with a cell in the target segmentation. Only cells with a minimum number of
@@ -401,7 +430,7 @@ def calculate_main_overlap_and_rest_one_cell(cell_id_source : int,
     cell_id_source : int
         Cell id of the cell in the source segmentation.
     spots_df : pandas.DataFrame
-        DataFrame with columns 'x', 'y', 'Gene', 'pixel_x', 'pixel_y', source_column_name and target_column_name.
+        DataFrame with columns gene_col_name, source_column_name, target_column_name.
     upper_threshold : float
         Upper threshold for share of spots in the main overlap. Default is 0.75.
     lower_threshold : float
@@ -428,7 +457,7 @@ def calculate_main_overlap_and_rest_one_cell(cell_id_source : int,
     spots_source_grouped = spots_source.groupby(target_column_name).count()
 
     # find out which target cell has the most spots in common with the source cell
-    cell_id_target = spots_source_grouped['Gene'].idxmax()
+    cell_id_target = spots_source_grouped[gene_col_name].idxmax()
 
     # main overlap spots
     spots_main_overlap = spots_source[spots_source[target_column_name] == cell_id_target]
@@ -443,13 +472,13 @@ def calculate_main_overlap_and_rest_one_cell(cell_id_source : int,
         return None, None
     else:
         # generate the list of all spots in the spots_df
-        all_spots = spots_df['Gene'].unique()
+        all_spots = spots_df[gene_col_name].unique()
         # generate the expression vectors for the main overlap and the rest
         expression_vector_main_overlap = np.zeros(len(all_spots))
         expression_vector_rest = np.zeros(len(all_spots))
-        for spot in spots_main_overlap['Gene']:
+        for spot in spots_main_overlap[gene_col_name]:
             expression_vector_main_overlap[np.where(all_spots == spot)] += 1
-        for spot in spots_rest['Gene']:
+        for spot in spots_rest[gene_col_name]:
             expression_vector_rest[np.where(all_spots == spot)] += 1
 
         return expression_vector_main_overlap, expression_vector_rest
