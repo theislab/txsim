6fb0269fb83ee6fc616d41e7780fecc85a591451
LouisK92 <37270609+LouisK92@users.noreply.github.com>
2024-05-13 10:40:18 +0200

    Clean up of some quality metrics

--- txsim/quality_metrics/_quality_metrics.py
+++ txsim/quality_metrics/_quality_metrics.py
@@ -3,24 +3,49 @@ import numpy as np
 import pandas as pd
 from anndata import AnnData
 from scipy.spatial import ConvexHull, convex_hull_plot_2d
+from scipy.sparse import issparse
+from typing import Optional
 
 
-def cell_density(adata_sp: AnnData,pipeline_output=True):
-    """Calculates the area of the region imaged using convex hull and divide total number of cells/area. XY position should be in um2"
+def cell_density(
+    adata_sp: AnnData,
+    scaling_factor: float = 1.0,
+    img_shape: Optional[tuple] = None,
+    pipeline_output: bool = True
+) -> float:
+    """Calculates the area of the region imaged using convex hull and divide total number of cells/area.
+    
     Parameters
     ----------
     adata_sp : AnnData
         annotated ``AnnData`` object with counts from spatial data
+    scaling_factor: float
+        XY position should be in µm. If not, a multiplicative scaling factor can be provided.
+    img_shape: tuple
+        Provide an image shape for area calculation instead of convex hull
     pipeline_output : float, optional
-        Boolean for whether to use the 
+        Generic argument for txsim metrics. Boolean for whether to return only the summary statistic or additional
+        metric specific outputs. (Here: no additional outputs)
+        
     Returns
     -------
     density : float
        Cell density (cells/um)
     """   
-    hull = ConvexHull(np.array(adata_sp.uns['spots'].loc[:,['x','y']]))
-    density=(adata_sp.shape[0]/hull.area)#*10e6
-    return density 
+    if scaling_factor == 1.0:
+        pos = adata_sp.uns['spots'].loc[:,['x','y']].values
+    else:
+        pos = adata_sp.uns['spots'].loc[:,['x','y']].values.copy() * scaling_factor
+    
+    if img_shape is not None:
+        area = img_shape[0] * img_shape[1]
+    else:
+        hull = ConvexHull(pos) #TODO: test for large dataset, maybe better to use cell positions for scalability
+        area = hull.area
+        
+    density= adata_sp.n_obs/area #*10e6 #TODO: Check if there can be numerical issues due to large area!!!
+    
+    return density
 
 def proportion_of_assigned_reads(adata_sp: AnnData,pipeline_output=True):
     """Proportion of assigned reads
@@ -29,47 +54,49 @@ def proportion_of_assigned_reads(adata_sp: AnnData,pipeline_output=True):
     adata_sp : AnnData
         annotated ``AnnData`` object with counts from spatial data
     pipeline_output : float, optional
-        Boolean for whether to use the 
+        Generic argument for txsim metrics. Boolean for whether to return only the summary statistic or additional
+        metric specific outputs. (Here: no additional outputs)
+        
     Returns
     -------
     proportion_assigned : float
        Proportion of reads assigned to cells / all reads decoded
-    """   
-    proportion_assigned=np.sum(adata_sp.layers['raw'])/adata_sp.uns['spots'].shape[0]
+    """
+    if issparse(adata_sp.layers['raw']):
+        proportion_assigned=adata_sp.layers['raw'].sum()/adata_sp.uns['spots'].shape[0]
+    else:
+        proportion_assigned=np.sum(adata_sp.layers['raw'])/adata_sp.uns['spots'].shape[0]
     return proportion_assigned
 
 
-def median_reads_cells(adata_sp: AnnData,pipeline_output=True):
-    """Median number of reads/cells
+def reads_per_cell(adata_sp: AnnData, statistic: str = "mean", pipeline_output=True):
+    """ Get mean/median number of reads per cell
+    
     Parameters
     ----------
     adata_sp : AnnData
-        annotated ``AnnData`` object with counts from spatial data
+        annotated ``AnnData`` object with counts from spatial data. Integer counts are expected in 
+        adata_sp.layers['raw'].
     pipeline_output : float, optional
-        Boolean for whether to use the 
+        Generic argument for txsim metrics. Boolean for whether to return only the summary statistic or additional
+        metric specific outputs. (Here: no additional outputs)
+        
     Returns
     -------
     median_cells : float
        Median_number_of_reads_x_cell
     """   
-    median_cells=np.median(np.sum(adata_sp.layers['raw'],axis=1))
-    return median_cells
+    if issparse(adata_sp.layers['raw']) and statistic == "mean":
+        return np.mean(adata_sp.layers['raw'].sum(axis=1))
+    elif issparse(adata_sp.layers['raw']) and statistic == "median":
+        return np.median(np.asarray(adata_sp.layers['raw'].sum(axis=1)).flatten())
+    elif statistic == "mean":
+        return np.mean(np.sum(adata_sp.layers['raw'],axis=1))
+    elif statistic == "median":
+        return np.median(np.sum(adata_sp.layers['raw'],axis=1))
+    else:
+        raise ValueError("Please choose either 'mean' or 'median' for statistic")
 
-def mean_reads_cells(adata_sp: AnnData,pipeline_output=True):
-    """Mean number of reads/cells
-    Parameters
-    ----------
-    adata_sp : AnnData
-        annotated ``AnnData`` object with counts from spatial data
-    pipeline_output : float, optional
-        Boolean for whether to use the 
-    Returns
-    -------
-    mean_cells : float
-       Mean_number_of_reads_x_cell
-    """   
-    mean_cells=np.mean(np.sum(adata_sp.layers['raw'],axis=1))
-    return mean_cells
 
 def number_of_genes(adata_sp: AnnData,pipeline_output=True):
     """ Size of the gene panel present in the spatial dataset
