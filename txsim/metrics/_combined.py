2ba35025ceb90c4134af483ac22d11309fb1122a
LouisK92 <37270609+LouisK92@users.noreply.github.com>
2024-06-10 13:02:53 +0200

    Add metrics to combined calc

--- txsim/metrics/_combined.py
+++ txsim/metrics/_combined.py
@@ -35,10 +35,12 @@ def all_metrics(
     ##metrics['relative_sim_across_gene_overall_metric'] = relative_pairwise_gene_expression(adata_sp, adata_sc, 'celltype', 'lognorm')
     ###metrics['mean_sim_across_clust'] = mean_similarity_gene_expression_across_clusters(adata_sp,adata_sc)
     ###metrics['prc95_sim_across_clust'] = percentile95_similarity_gene_expression_across_clusters(adata_sp,adata_sc)
+    metrics['rel_pairwise_ct_expr_sim'] = relative_pairwise_celltype_expression(adata_sp.copy(), adata_sc.copy())
+    metrics['rel_pairwise_gene_expr_sim'] = relative_pairwise_gene_expression(adata_sp.copy(), adata_sc.copy())
     #
     ### Coexpression similarity
-    #metrics['coexpression_similarity'] = coexpression_similarity(adata_sp, adata_sc)
-    #metrics['coexpression_similarity_celltype'] = coexpression_similarity(adata_sp, adata_sc, by_celltype = True)
+    metrics['coexpr_similarity'] = coexpression_similarity(adata_sp.copy(), adata_sc.copy())
+    metrics['coexpr_similarity_celltype'] = coexpression_similarity(adata_sp.copy(), adata_sc.copy(), by_celltype = True)
     ##metrics['gene_set_coexpression'] = gene_set_coexpression(adata_sp, adata_sc)
         
     # Negative marker purity
@@ -46,7 +48,7 @@ def all_metrics(
     metrics['neg_marker_purity_reads'] = negative_marker_purity_reads(adata_sp.copy(),adata_sc.copy())
     
     #### KNN mixing
-    #metrics['knn_mixing'] = knn_mixing(adata_sp.copy(),adata_sc.copy())
+    metrics['knn_mixing'] = knn_mixing(adata_sp.copy(),adata_sc.copy())
     
     # Cell statistics
     #metrics['ratio_median_readsxcell'] = ratio_median_readsXcells(adata_sp,adata_sc)
--- txsim/quality_metrics/_combined.py
+++ txsim/quality_metrics/_combined.py
@@ -17,16 +17,18 @@ def all_quality_metrics(
     
     metrics['cellular_density']=cell_density(adata_sp)
     metrics['prop_reads_assigned']=proportion_of_assigned_reads(adata_sp)
-    metrics['median_readsxcell']=median_reads_cells(adata_sp)
-    metrics['mean_readsxcell']=mean_reads_cells(adata_sp)
+    metrics['mean_reads_per_cell']=reads_per_cell(adata_sp, statistic='mean')
+    metrics['median_reads_per_cell']=reads_per_cell(adata_sp, statistic='median')
+    metrics['mean_genes_per_cell']=genes_per_cell(adata_sp, statistic='mean')
+    metrics['median_genes_per_cell']=genes_per_cell(adata_sp, statistic='median')
     metrics['number_of_genes']=number_of_genes(adata_sp)
     metrics['number_of_cells']=number_of_cells(adata_sp)
-    metrics['pct5_readsxcell']=percentile_5th_reads_cells(adata_sp)
-    metrics['mean_genesxcell']=mean_genes_cells(adata_sp)
-    metrics['pct95_genesxcell']=percentile_95th_genes_cells(adata_sp)
-    metrics['pct5_genesxcell']=percentile_5th_genes_cells(adata_sp)
-    metrics['median_genexcell']=median_genes_cells(adata_sp)
-    metrics['pct95_readsxcell']=percentile_95th_reads_cells(adata_sp)
+    #metrics['pct5_readsxcell']=percentile_5th_reads_cells(adata_sp)
+    #metrics['mean_genesxcell']=mean_genes_cells(adata_sp)
+    #metrics['pct95_genesxcell']=percentile_95th_genes_cells(adata_sp)
+    #metrics['pct5_genesxcell']=percentile_5th_genes_cells(adata_sp)
+    #metrics['median_genexcell']=median_genes_cells(adata_sp)
+    #metrics['pct95_readsxcell']=percentile_95th_reads_cells(adata_sp)
     
     
     
