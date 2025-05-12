602981b7dc6bd273d8f0fe47026f1cbd79484938
Habib Rehman <harehman@iu.edu>
2025-05-12 00:58:52 -0400

    added different correlation methods and a QC filter for noisy genes

--- txsim/metrics/_relative_gene_expression.py
+++ txsim/metrics/_relative_gene_expression.py
@@ -10,47 +10,70 @@ def relative_gene_expression(
     adata_sc: AnnData, 
     cell_type_key:str='cell_type',
     layer:str='normalized',  
-    min_cells:int = 10,  
+    min_cells:int = 10, 
+    method:str =  'spearman',
+    min_cells_expressing_gene:float = 0.01
 ) -> float:
-    """Calculate the efficiency deviation present between the genes in the panel. 
+    """Calculate the correlation between the gene expression in scRNAseq and spatial data, averaged across cell types
     ----------
     adata_sp : AnnData
-        annotated ``AnnData`` object with counts from spatial data
+        Annotated ```AnnData``` object with counts from spatial data
     adata_sc : AnnData
-        annotated ``AnnData`` object with counts from scRNAseq data
+        Annotated ```AnnData``` object with counts from scRNAseq data
     cell_type_key: str (default: 'cell_type')
-        .obs column of ``AnnData`` that contains celltype information
+        Column of ```.obs``` in ```AnnData``` that contains celltype information
     layer: str (default: 'normalized')
-        layer of ```AnnData`` to use to compute the metric
+        Layer of ```AnnData`` to use to compute the metric
     min_cells: int (default: 10)
-        minimum number of cells needed for a cell type to be included
+        Minimum number of cells in a cell type needed for that cell type to be included
+    method: str (default: 'spearman')
+        Method use to calculate correlation, options are 'spearman', 'pearson', and 'kendall' (for Kendall's tau-b)
+    min_cells_expressing_gene: float (default: 0.01)
+        Minimum fraction of cells in that cell type expressing a gene needed for that gene to be included in correlation.
+        By default, genes expressed in <1% of cells for that cell type will not be used to calculate correlation. 
+        Pass in 0 to calculate correlation across all genes- note this may result in zero-inflated data. 
 
     Returns
     -------
     average_corr: float
-        Spearman correlation of gene expression for same cell type in scRNA-seq vs spatial data. Simple average across all cell types
+        Correlation of gene expression for same cell type in scRNA-seq vs spatial data. Simple average across all cell types
   
     """   
     from scipy import stats
 
     common_cell_types = [x for x in adata_sp.obs['cell_type'].unique() if x in adata_sc.obs['cell_type'].unique()] 
-    spearman_corr = pd.Series(data=0, index = common_cell_types, dtype = np.float64)
+    correlations = pd.Series(data=0, index = common_cell_types, dtype = np.float64)
 
-    # for each cell type, calculate spearman or NaN if below min_cells
+    # for each cell type, calculate correlation or NaN if below min_cells
     for cell_type in common_cell_types:
 
         spatial_cells = adata_sp[adata_sp.obs['cell_type'] == cell_type]
         scrnaseq_cells = adata_sc[adata_sc.obs['cell_type'] == cell_type]
         
         #skip cell types with fewer than min_cells
-        if len(spatial_cells.obs.index) < min_cells or len(scrnaseq_cells.obs.index) < min_cells: 
-            spearman_corr[cell_type] = np.nan
+        if len(spatial_cells.obs.index) < min_cells: # or len(scrnaseq_cells.obs.index) < min_cells: #TODO um idk here
+            correlations[cell_type] = np.nan
             continue 
 
         avg_gene_expression_ref = np.average(scrnaseq_cells.layers['normalized'].toarray(), axis=0) #TODO can we assume normalized layer will be sparse?
-        avg_gene_expression_spatial = np.average(spatial_cells.layers['normalized'].toarray(), axis=0)
+        avg_gene_expression_spatial = np.average(spatial_cells.layers['normalized'].toarray(), axis=0) #otherwise 'toarray' is not needed
 
-        spearman_corr[cell_type] = stats.spearmanr(avg_gene_expression_ref, avg_gene_expression_spatial).statistic
+        #calculate fraction of cells expressing each gene
+        cell_fraction = np.average(scrnaseq_cells.layers['normalized'].toarray()>0, axis=0)
 
-    #return mean of spearman, ignoring NaN's
-    return np.nanmean(spearman_corr)
+        #filter out genes below threshold
+        filter_idx = np.where(cell_fraction >= min_cells_expressing_gene)
+        avg_gene_expression_ref = avg_gene_expression_ref[filter_idx]
+        avg_gene_expression_spatial = avg_gene_expression_spatial[filter_idx]
+
+        if ('spearman' in method):
+            correlations[cell_type] = stats.spearmanr(avg_gene_expression_ref, avg_gene_expression_spatial).statistic
+        elif ('kendall' in method):
+            correlations[cell_type] = stats.kendalltau(avg_gene_expression_ref, avg_gene_expression_spatial).statistic
+        elif ('pearson' in method): 
+            correlations[cell_type] = stats.pearsonr(avg_gene_expression_ref, avg_gene_expression_spatial).statistic
+        else:
+            print("invalid method used") #TODO make this warning better
+
+    #return mean of correlations, ignoring NaN's
+    return np.nanmean(correlations)
